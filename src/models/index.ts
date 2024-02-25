7547157c47d73bf021b06a69d2399ed71e06c9b5
megaxd274 <kazemirova.l@gmail.com>
2024-02-25 21:06:36 +0300

    feat: add Reg functionality and corresponding types

---
+++ src/models/index.ts
@@ -0,0 +1,26 @@
+export enum Commands {
+  reg = 'reg',
+  update_winners = 'update_winners',
+  create_room = 'create_room',
+  add_user_to_room = 'add_user_to_room',
+  create_game = 'create_game',
+  update_room = 'update_room',
+  add_ships = 'add_ships',
+  start_game = 'start_game',
+  attack = 'attack',
+  randomAttack = 'randomAttack',
+  turn = 'turn',
+  finish = 'finish',
+}
+export interface IRegResponseData {
+  name: string;
+  index: number | string;
+  error: boolean;
+  errorText: string;
+}
+
+export interface IResponse {
+  type: Commands;
+  data: string;
+  id: 0;
+}
--- src/ws_server/server.ts
+++ src/ws_server/server.ts
@@ -1,10 +1,29 @@
 import { WebSocketServer } from 'ws';
+import { IResponse, Commands, IRegResponseData } from '../models/index.js';
+import { IUser, userStorage } from '../storages/userStorage.js';
 
 const server = new WebSocketServer({ port: 3000 });
 
 server.on('connection', (ws) => {
   console.log('new client conntected');
   ws.on('message', (data) => {
-    console.log(JSON.parse(data.toString()));
+    const response: IResponse = JSON.parse(data.toString());
+    if (response.type === Commands.reg) {
+      const userinfo: IUser = JSON.parse(response.data);
+      userStorage.push(userinfo);
+      const index = userStorage.findIndex((value) => value === userinfo);
+      const serverResponseData: IRegResponseData = {
+        name: userinfo.name,
+        index,
+        error: false,
+        errorText: 'asdasd',
+      };
+      const serverResponse: IResponse = {
+        type: Commands.reg,
+        data: JSON.stringify(serverResponseData),
+        id: 0,
+      };
+      ws.send(JSON.stringify(serverResponse));
+    }
   });
 });
